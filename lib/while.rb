8f6eba5653da80558672916a38da9f7ab72bfa16
clifford <clifford@noukod.com>
2018-08-31 18:02:39 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
 def using_while
 	levitation_force = 6
 	 while levitation_force < 10
-	 puts "The current number is less than 10."
+	 puts "The current levitation is less than 10."
 	#your code here
 	levitation_force += 1
 end

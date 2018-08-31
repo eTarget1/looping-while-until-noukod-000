8b6023eb21f55c7250f0e2e0a43a8705df959ea2
clifford <clifford@noukod.com>
2018-08-31 18:06:13 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,10 +1,9 @@
 def using_until
         levitation_force = 6
         #your code here
-        until levitation_force < 10
+        until levitation_force <= 10
         puts "The Wingardium Leviosa #{levitation_force}"
        #your code here
        levitation_force += 1
-    
-end
 
+end
